09717fda21814f70901675079054a67f1898981c
kaorinS <pluvia.seed@icloud.com>
2020-04-04 10:42:19 +0900

    headerのhtml,css完了

--- head.php
+++ head.php
@@ -4,4 +4,8 @@
     <meta charset="UTF-8" />
     <title><?php echo $title ?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/30d61d93fd.js" crossorigin="anonymous"></script>
+    <!-- google フォント -->
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
   </head>
