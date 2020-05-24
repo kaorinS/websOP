fd2e8bec9a96adb95f31afc8e0f034f7b3d50aac
kaorinS <pluvia.seed@icloud.com>
2020-05-24 21:39:34 +0900

    php ヘッダー分岐　完了

--- header.php
+++ header.php
@@ -3,7 +3,12 @@
         <h1 class="logo"><a href="index.php"><i class="fab fa-fly"></i>イベ探！</a></h1>
         <nav class="header-nav">
             <ul class="header-ul">
-                <li class="header-li"><a href="login.php">新規登録 / ログイン</a></li>
+                <?php if (empty($_SESSION['user_id'])) : ?>
+                    <li class="header-li"><a href="login.php">新規登録 / ログイン</a></li>
+                <?php else : ?>
+                    <li class="header-li -login"><a href="mypage.php">マイページ</a></li>
+                    <li class="header-li -login"><a href="#">ログアウト</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
     </div>
